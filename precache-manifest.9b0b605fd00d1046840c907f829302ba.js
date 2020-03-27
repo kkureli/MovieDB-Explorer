2a91254402309fcccac4b712f34c0ceb0fd8ede4
Kaan <kkureli@gmail.com>
2020-03-27 21:54:57 +0300

    Updates

---
+++ precache-manifest.9b0b605fd00d1046840c907f829302ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12230c6294691ff0cc281ba854e60c5",
+    "url": "/MovieExplorer/index.html"
+  },
+  {
+    "revision": "0b5a35a89a228586cbad",
+    "url": "/MovieExplorer/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c3d88790cb53b5a31fb1",
+    "url": "/MovieExplorer/static/css/main.4db4e354.chunk.css"
+  },
+  {
+    "revision": "0b5a35a89a228586cbad",
+    "url": "/MovieExplorer/static/js/2.90bb85cd.chunk.js"
+  },
+  {
+    "revision": "a25cc1a7735e44380bf097b418b6377c",
+    "url": "/MovieExplorer/static/js/2.90bb85cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3d88790cb53b5a31fb1",
+    "url": "/MovieExplorer/static/js/main.300347b6.chunk.js"
+  },
+  {
+    "revision": "9d32264b8e7311a96d99",
+    "url": "/MovieExplorer/static/js/runtime-main.f67a818e.js"
+  }
+]);
