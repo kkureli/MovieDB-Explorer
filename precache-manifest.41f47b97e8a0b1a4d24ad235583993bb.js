830d3acdfd11497aac007de4f2bba96f4b6e2c32
Kaan <kkureli@gmail.com>
2020-04-06 18:43:37 +0300

    Updates

--- precache-manifest.41f47b97e8a0b1a4d24ad235583993bb.js
+++ precache-manifest.41f47b97e8a0b1a4d24ad235583993bb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "37152e29fea0aa98a1fa109fc4e88bdb",
+    "revision": "353142c31fad3a6b31f36274ddfa4194",
     "url": "/MovieExplorer/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MovieExplorer/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "827b261b78875bc07fb6",
+    "revision": "a3f12f0d611d4555028f",
     "url": "/MovieExplorer/static/css/main.4db4e354.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/MovieExplorer/static/js/2.c256e158.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "827b261b78875bc07fb6",
-    "url": "/MovieExplorer/static/js/main.fbc0e9f7.chunk.js"
+    "revision": "a3f12f0d611d4555028f",
+    "url": "/MovieExplorer/static/js/main.41072ea6.chunk.js"
   },
   {
     "revision": "9d32264b8e7311a96d99",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MovieExplorer/precache-manifest.b4b8647854286aca14053f2f3848c615.js"
+  "/MovieExplorer/precache-manifest.41f47b97e8a0b1a4d24ad235583993bb.js"
 );
 
 self.addEventListener('message', (event) => {
